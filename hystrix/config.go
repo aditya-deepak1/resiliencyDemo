5d41e3bb0f24b8b02034b673cd881822c0ed29a1
Aditya Deepak <94464655+aditya-deepak1@users.noreply.github.com>
2022-08-10 16:23:03 +0530

    feat: adding hystrix demo handler (#1)
    * adding hystrix demo handler for demo
    
    * Update .gitignore

---
+++ hystrix/config.go
@@ -0,0 +1,18 @@
+package hystrix
+
+import (
+	hgo "github.com/afex/hystrix-go/hystrix"
+)
+
+func ConfigureHystrix() {
+	
+	// Configure test command.
+	// Default rolling window is 10s.
+	hgo.ConfigureCommand(TestCommand, hgo.CommandConfig{
+		Timeout:                350, // TimeoutMs
+		MaxConcurrentRequests:  1,
+		RequestVolumeThreshold: 10,
+		SleepWindow:            15000, // SleepWindowMs
+		ErrorPercentThreshold:  20,
+	})
+}
--- main.go
+++ main.go
@@ -1,15 +1,19 @@
 package main
 
 import (
-	"context"
-	"fmt"
 	"net/http"
-	"time"
+	
+	"resiliencyDemo/context"
+	"resiliencyDemo/hystrix"
 )
 
 func main() {
 	// mount the handler.
-	http.HandleFunc("/add", add)
+	http.HandleFunc("/context", context.Handler)
+	http.HandleFunc("/hystrix", hystrix.Handler)
+	
+	// Configure hystrix commands.
+	hystrix.ConfigureHystrix()
 	
 	// ask http server to start at 8080 port.
 	server := http.Server{
@@ -20,65 +24,5 @@ func main() {
 	err := server.ListenAndServe()
 	if err != nil {
 		panic("server crashed")
-	}
-}
-
-func add(w http.ResponseWriter, r *http.Request) {
-	
-	ctx, cancel := context.WithTimeout(r.Context(), time.Second*10)
-	defer cancel()
-	
-	go func() {
-		select {
-		
-		case <-time.After(time.Second * 20):
-			fmt.Println("overslept")
-		
-		case <-ctx.Done():
-			fmt.Println("Inside go routine")
-			fmt.Println(ctx.Err())
-		}
-	}()
-	
-	// make external call.
-	makeHTTPRequest(ctx)
-	
-	_, err := w.Write([]byte("Addition Done"))
-	if err != nil {
-		return
-	}
-	w.WriteHeader(http.StatusOK)
-}
-
-func makeHTTPRequest(ctx context.Context) {
-	
-	select {
-	
-	case <-ctx.Done():
-		fmt.Println("Inside makeHTTRequest func")
-		fmt.Println(ctx.Err())
-	
-	case <-time.After(time.Second * 5):
-		fmt.Println("calling Google.com")
-		
-		client := &http.Client{
-			Timeout: time.Second * 60,
-		}
-		
-		req, err := http.NewRequest("GET", "https://google.com", nil)
-		if err != nil {
-			fmt.Println("Error forming HTTP request")
-		}
-		
-		req.WithContext(ctx)
-		start := time.Now()
-		_, err = client.Do(req)
-		
-		if err != nil {
-			fmt.Println("Error calling Google.com", err)
-		}
-		elapsedTime := time.Since(start).Milliseconds()
-		
-		fmt.Printf("Done calling Google.com in %d ms\n", elapsedTime)
 	}
 }
